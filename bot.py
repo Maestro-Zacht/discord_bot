f39e5f18e78dcd47a70669d678451d38fff9e3e0
Matteo Ghia <matteo.ghia.2001@gmail.com>
2020-07-31 17:39:12 +0200

    can read from direct message

--- bot.py
+++ bot.py
@@ -20,13 +20,54 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author != client.user and message.guild.name == GUILD and message.channel.name == CHANNEL and len(message.attachments) != 0:
-        for file in message.attachments:
-            with open('allegato.csv', 'wb') as f:
-                await file.save(f)
-            with open('allegato.csv') as f:
-                with open('file.csv', 'w') as wri:
-                    wri.write(f.read().replace('\t', ','))
+    if message.author != client.user and message.guild.name == GUILD and message.channel.name == CHANNEL:
+        print('messaggio ricevuto, elaborazione...')
+        if len(message.attachments) != 0:
+            for file in message.attachments:
+                with open('allegato.csv', 'wb') as f:
+                    await file.save(f)
+                with open('allegato.csv') as f:
+                    with open('file.csv', 'w') as wri:
+                        wri.write(f.read().replace('\t', ','))
+                df = pd.read_csv('file.csv')
+                players = set(df['Pilot'])
+                giocatori = {}
+                for i in players:
+                    giocatori[i] = {'volume': 0, 'percentuale': 0}
+                totale_volume = 0
+                for riga in df.itertuples(name='Riga'):
+                    giocatori[riga.Pilot]['volume'] += riga.Volume
+                    totale_volume += riga.Volume
+
+                for val in giocatori.values():
+                    val['percentuale'] = val['volume'] / totale_volume * 100
+
+                fig, ax = plt.subplots()
+
+                dati_y = list(giocatori.keys())
+                dati_x = []
+                for giocatore in giocatori.values():
+                    dati_x.append(giocatore['volume'])
+
+                ax.barh(dati_y, dati_x)
+
+                ax.set_xlabel('Volume (metri cubi)')
+                ax.set_ylabel('Pilot')
+
+                fig.savefig('prova.jpg')
+                plt.close(fig)
+
+                await message.channel.send(file=discord.File('prova.jpg'))
+
+                testo = 'Percentuali di raccolta:\n'
+                for giocatore, val in giocatori.items():
+                    testo += f"{giocatore}:\t{val['percentuale']} %\n"
+
+                await message.channel.send(testo)
+        else:
+            with open('file.csv', 'w') as f:
+                f.write(message.content.replace('    ', ','))
+
             df = pd.read_csv('file.csv')
             players = set(df['Pilot'])
             giocatori = {}
@@ -62,5 +103,6 @@ async def on_message(message):
                 testo += f"{giocatore}:\t{val['percentuale']} %\n"
 
             await message.channel.send(testo)
+        print('messaggio elaborato')
 
 client.run(TOKEN)
